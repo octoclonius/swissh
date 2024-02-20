bd7e4b7b74091a9a4faace31bc6d3d25c1cb144b
octoclonius <25781800+octoclonius@users.noreply.github.com>
2024-02-20 12:30:45 -0500

    Secure WebSocket progress

---
+++ frontend/src/WISSHTerminal.js
@@ -0,0 +1,67 @@
+import React, { useEffect } from 'react';
+import { Terminal } from '@xterm/xterm';
+import { FitAddon } from '@xterm/addon-fit';
+import { WebglAddon } from '@xterm/addon-webgl';
+import { AttachAddon } from '@xterm/addon-attach';
+import './xterm.css';
+
+function WISSHTerminal() {
+  useEffect(() => {
+    const terminal = new Terminal();
+    terminal.open(document.getElementById("terminal"));
+
+    const xtermWebglAddon = new WebglAddon();
+    try {
+      xtermWebglAddon.onContextLoss(() => {
+        xtermWebglAddon.dispose();
+      });
+      terminal.loadAddon(xtermWebglAddon);
+    } catch (err) {
+      console.warn("Failed to load xterm WebGL addon", err);
+      xtermWebglAddon.dispose();
+    }
+
+    const xtermFitAddon = new FitAddon();
+    try {
+      terminal.loadAddon(xtermFitAddon);
+    } catch (err) {
+      console.error("Failed to load xterm fit addon", err);
+      xtermFitAddon.dispose();
+    }
+
+    const ws = new WebSocket('wss://127.0.0.1:8000/wissh');
+    ws.onopen = () => {
+      xtermFitAddon.fit();
+      ws.send(JSON.stringify({ resize: { cols: terminal.cols, rows: terminal.rows } }));
+    };
+
+    const xtermAttachAddon = new AttachAddon(ws);
+    try {
+      terminal.loadAddon(xtermAttachAddon);
+    } catch (err) {
+      console.error("Failed to load xterm attach addon", err);
+      xtermAttachAddon.dispose();
+      ws.close();
+    }
+
+    const handleResize = () => {
+      xtermFitAddon.fit();
+      ws.send(JSON.stringify({ resize: { cols: terminal.cols, rows: terminal.rows } }));
+    };
+
+    window.addEventListener("resize", handleResize);
+
+    return () => {
+      terminal.dispose();
+      xtermWebglAddon.dispose();
+      xtermFitAddon.dispose();
+      xtermAttachAddon.dispose();
+      ws.close();
+      window.removeEventListener("resize", handleResize);
+    };
+  }, []);
+
+  return <div id="terminal" />;
+};
+
+export default WISSHTerminal;
