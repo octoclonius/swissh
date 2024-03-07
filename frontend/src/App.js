7940d5966cad127d9ed956ad92fed93094755179
angelabannan <angelabannan@yahoo.com>
2024-03-07 03:20:00 -0500

    navbar&files

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,7 @@ import WISSHTerminal from './WISSHTerminal';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Navbar from "./components/navbar/navbar"
 import Machine from "./Pages/navbar/machine"
+import File from "./components/File/file"
 function App() {
   return (
     <div className="App">
@@ -11,6 +12,7 @@ function App() {
           <Route path='/machine' exact component={Machine} />
         </Routes>
       </Router>
+      
       <WISSHTerminal />
     </div>
   );
--- frontend/src/components/navbar/navbar.js
+++ frontend/src/components/navbar/navbar.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import { Link } from "react-router-dom";
+import "./navbar.css"
+import File from "../File/file"
+import star from './star.png';
+
 const Navbar = () =>{
   return (
-  <div>
-    <li>
-      <Link to="/machine">Machine</Link>
-    </li>
-    <li>
-      <Link to="/">terminal</Link>
-    </li>
+    <div className= "outer-container">
+    <div className="flex-container1">
+      <nav>
+      <div className = "inner-flex-container">
+        <div>
+          <img src={star} alt="starImage" className='star' />
+        </div>
+        <div>
+          <Link to="/machine" className='link'>Machine</Link>
+          <hr className='line' />
+        </div>
+        <div>
+          <Link to="/" className='link'>Terminal</Link>
+          <hr className='line' />
+        </div>
+      </div>
+      </nav>
+    </div>
+    <div className="flex-container2">
+      <File />
+    </div>
   </div>
   );
 }
